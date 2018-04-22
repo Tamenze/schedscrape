ba718d2ec5e34738589bc0b3a6f2ab7aabf7ce82
Tolulope Edionwe <tamenze@gmail.com>
2018-04-21 22:23:07 -0400

    Updated python translation of peridaily

--- pythonTranslation/peridaily.py
+++ pythonTranslation/peridaily.py
@@ -1,11 +1,10 @@
 #! /usr/bin/env python3
 
-import urllib.request as urllib2
+import requests
 from bs4 import BeautifulSoup
 
-class_page = 'http://www.peridance.com/openclasses.cfm'
-page = urllib2.urlopen(class_page)
-soup = BeautifulSoup(page, 'html.parser')
+page = requests.get('http://www.peridance.com/openclasses.cfm')
+soup = BeautifulSoup(page.text, 'html.parser')
 
 date = soup.table.tr.span.text.split('for: ')[1]
 genreList = soup.find_all('tr')
@@ -13,26 +12,26 @@ genreList = soup.find_all('tr')
 
 #print(date) #prints date of class
 for x in genreList:
-    print(x)
+    #print(x)
     if x.td.center:
-        print(x.td.center.text) #prints genre of class 
+        print("genre: " + x.td.center.text) #prints genre of class 
     elif x.td.text:
-        infoList = x.find_all('td')
-        for y in infoList:
-            if y.text:
-                print(y.text) #prints time of class
-            elif y.div.text:
-                print(y.div.text.strip()) #prints level of class
-            elif y.a.font.text:
-                print(y.a.font.text) #prints teacher of class
-    #if x.td.next_sibling: #will never work bc using a tag name as an attr
-        #only gets u the first tag by that name
+        infoList = x.find_all('td') #finds all td in current tr (3 total)
+        for cell in infoList:
+            if cell.attrs: #skips printing for time/level/teacher rows
+                if cell.string: #if cell has only child and child is a navigable string, returns as string
+                    print("time: " + str(cell.string))
+                elif cell.div and cell.div.text:
+                    print("level: " + str(cell.div.text).strip())
+                elif cell.a:
+                    print("link: " + "http://www.peridance.com/" + str(cell.a['href']) + "\n" + "teacher: " + str(cell.font.text))
+                elif cell.font and cell.font.text:
+                     print("teacher: " + str(cell.text).strip())
+ #                   print("OUTLIER: " + str(cell))
     print("\n-------")
 
 
 
 #NEXT STEPS
-#strip time/level/teacher td
-#strip whitespace
 #replace list with dictionary/object-like structure
 #translate these objects into records for Database
